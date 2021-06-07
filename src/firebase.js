6be187dc30471c6f2475be87256c83cd7abbca40
SaheenJasmine <63956055+developweb-Sah@users.noreply.github.com>
2021-06-08 00:31:44 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@
 
 import firebase from "firebase";
 const firebaseConfig = {
-    apiKey: "AIzaSyBSLIflAXGHhswh6NU6HpPBB2lFAkkwe5Q",
-    authDomain: "twitter-clone-a609b.firebaseapp.com",
-    projectId: "twitter-clone-a609b",
-    storageBucket: "twitter-clone-a609b.appspot.com",
-    messagingSenderId: "96205234211",
-    appId: "1:96205234211:web:6f8f19f1d7939bcd755196",
-    measurementId: "G-MSB46N6B9T"
+    apiKey: "AIzaSyBSLIflAXqGHhswh6NU6HpPBB2lFAwkkwe5Q",
+    authDomain: "twitter-clone-a623b.firebaseapp.com",
+    projectId: "twitter-clone-a623b",
+    storageBucket: "twitter-clone-a623b.appspot.com",
+    messagingSenderId: "96205234231",
+    appId: "1:96205234211:web:6f8f19f1d7939bfd755196",
+    measurementId: "G-MSB4GN6B9T"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
